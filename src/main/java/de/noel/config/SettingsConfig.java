9d446fe076ad438797cf1e7a2cc3ce0076968242
DorSchmidi <noelschmieder01@gmail.com>
2021-01-19 22:37:23 +0100

    updating current state

--- src/main/java/de/noel/config/SettingsConfig.java
+++ src/main/java/de/noel/config/SettingsConfig.java
@@ -14,6 +14,7 @@ import de.noel.model.ActivatedTracker;
 
 public class SettingsConfig {
 
+	private final String defaultPath = "settings.";
 	private File customConfigFile;
 	private FileConfiguration customConfig;
 	private Start plugin;
@@ -24,11 +25,11 @@ public class SettingsConfig {
 	}
 
 	private void createCustomConfig() {
-		customConfigFile = new File(plugin.getDataFolder(), "settings.yml");
+		customConfigFile = new File(plugin.getDataFolder(), defaultPath+"yml");
 		if (!customConfigFile.exists()) {
 			customConfigFile.getParentFile().mkdirs();
 			this.loadDefaultValues();
-			plugin.saveResource("settings.yml", false);
+			plugin.saveResource(defaultPath+"yml", false);
 		}
 
 		customConfig = new YamlConfiguration();
@@ -41,14 +42,14 @@ public class SettingsConfig {
 
 	private void loadDefaultValues() {
 
-		this.customConfig.set("settings.defaultFood", true);
+		this.customConfig.set(defaultPath+"defaultFood", true);
+		this.customConfig.set(defaultPath+"loosingFoodLevel", 1);
+		this.customConfig.set(defaultPath+"maxPoints", 1500);
 
-		this.customConfig.set("settings.maxPoints", 1500);
-
-		this.customConfig.set("settings.points.BLOCK_DESTROYING", 100);
-		this.customConfig.set("settings.points.BLOCK_PLACING", 100);
-		this.customConfig.set("settings.points.SLEEPING", 10000);
-		this.customConfig.set("settings.points.WALKING", 100);
+		this.customConfig.set(defaultPath+"points.BLOCK_DESTROYING", 100);
+		this.customConfig.set(defaultPath+"points.BLOCK_PLACING", 100);
+		this.customConfig.set(defaultPath+"points.SLEEPING", 10000);
+		this.customConfig.set(defaultPath+"points.WALKING", 100);
 
 	}
 
@@ -61,15 +62,19 @@ public class SettingsConfig {
 	}
 	
 	public boolean usingDefaultFood() {
-		return this.customConfig.getBoolean("settings.defaultFood");
+		return this.customConfig.getBoolean(defaultPath+"defaultFood");
+	}
+	
+	public int getLossingFoodLevel() {
+		return this.customConfig.getInt(defaultPath+"loosingFoodLevel");
 	}
 	
 	public double getMaxPoints() {
-		return this.customConfig.getDouble("settings.maxPoints");
+		return this.customConfig.getDouble(defaultPath+"maxPoints");
 	}
 	
 	public double getPointsFrom(String tracker) {
-		return this.customConfig.getDouble("settings.points."+tracker);
+		return this.customConfig.getDouble(defaultPath+"points."+tracker);
 	}
 	
 	public Map<ActivatedTracker, Double> getAllTrackerPoints() {
